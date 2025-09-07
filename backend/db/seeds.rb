9a1b24497a350a5bbc9b57eeab9d2f0e61e73a44
Caitlyn Landry <caitlyn.landry@gmail.com>
2025-09-07 19:41:02 -0400

    Add seed data

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -7,3 +7,26 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require "faker"
+
+puts "Seeding properties..."
+
+Property.destroy_all
+
+US_STATES = Property::US_STATES
+
+20.times do
+  Property.create!(
+    name: Faker::Company.name,
+    address1: Faker::Address.street_address,
+    address2: [Faker::Address.secondary_address, nil].sample,
+    city: Faker::Address.city,
+    state: US_STATES.sample,
+    zip: Faker::Address.zip_code,
+    year_built: rand(1800..Date.current.year),
+    website_url: Faker::Internet.url
+  )
+end
+
+puts "Created #{Property.count} properties."
+
