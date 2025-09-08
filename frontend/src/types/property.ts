dbf21bbbbf6b2d72cf8e34f804fd1eb3bf5fd37a
Caitlyn Landry <caitlyn.landry@gmail.com>
2025-09-08 17:37:42 -0400

    Add Property type

---
+++ frontend/src/types/property.ts
@@ -0,0 +1,11 @@
+export interface Property {
+  id: number
+  name: string
+  address1: string
+  address2?: string
+  city: string
+  state: string
+  zip: string
+  year_built?: number
+  website_url?: string
+}
